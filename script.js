46ee79a1a348d2a0d66307b4a0b9dc04a28c8755
Darrel Toledo <darreltoledo@icloud.com>
2022-04-05 21:57:37 +0100

    init DOM in js

---
+++ script.js
@@ -0,0 +1,4 @@
+const quoteText = document.getElementById ('quote-text'),
+      quoteTags = document.getElementById ('quote-tags'),
+      quoteAuthor = document.getElementById ('quote-author'),
+      genQuoteBtn = document.getElementById ('gen-quote-btn');
