fb1632507292f185eff206ea53edef9e011ee681
Darrel Toledo <darreltoledo@icloud.com>
2022-04-06 19:00:02 +0100

    fetch api for quotable random

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const quoteText = document.getElementById ('quote-text'),
       quoteTags = document.getElementById ('quote-tags'),
       quoteAuthor = document.getElementById ('quote-author'),
       genQuoteBtn = document.getElementById ('gen-quote-btn');
+
+fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.content);
+    })
