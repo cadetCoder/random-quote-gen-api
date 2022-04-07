22e4a0a1d49cffe0f2c1e1833febca3ded57ebff
Darrel Toledo <darreltoledo@icloud.com>
2022-04-07 23:07:14 +0100

    fetch api

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ const quoteText = document.getElementById ('quote-text'),
 fetch('https://api.quotable.io/random')
     .then(response => response.json())
     .then(data => {
-      console.log(data.content);
+      quoteText.textContent = data.content;
     })
